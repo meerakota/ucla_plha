625050ea3c681ccf25f479b498352aba4dd415fa
sjbrandenberg <sjbrandenberg@ucla.edu>
2025-01-28 15:07:17 -0800

    Update cetin_et_al_2018.py

--- src/ucla_plha/liquefaction_models/cetin_et_al_2018.py
+++ src/ucla_plha/liquefaction_models/cetin_et_al_2018.py
@@ -15,7 +15,7 @@ def get_csr(mu_ln_pga, sigma_ln_pga, m, sigmav, sigmavp, vs12, d):
     sigma_ln_csr = sigma_ln_pga
     return [mu_ln_csr, sigma_ln_csr]
 
-def get_fsl_hazards(mu_ln_pga, sigma_ln_pga, fsl, m, sigmav, sigmavp, vs12, d, n160, fc, pa):
+def get_fsl_cdfs(mu_ln_pga, sigma_ln_pga, fsl, m, sigmav, sigmavp, vs12, d, n160, fc, pa):
     mu_ln_crr = (n160*(1+0.00167*fc) - 27.352 * np.log(m) - 3.958 * np.log(sigmavp / pa) + 0.089 * fc + 16.084)/11.771
     sigma_ln_crr = np.full(len(m), 2.95 / 11.771)
     rd_num = 1.0 + (-23.013 - 2.949 * np.exp(mu_ln_pga) + 0.999 * m + 0.0525 * vs12) / (16.258 + 0.201 * np.exp(0.341 * (-d + 0.0785 * vs12 + 7.586)))
